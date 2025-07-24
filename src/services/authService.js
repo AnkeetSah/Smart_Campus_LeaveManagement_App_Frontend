e965a2f04038bf1426255223119560e39650631f
AnkeetSah <ankeetkumar7777@gmail.com>
2025-07-24 22:28:07 +0530

    update the backend url

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,16 @@
 // src/services/authService.js
 import axios from "axios";
 
+const API_BASE = import.meta.env.VITE_API_URL;
+
 export const loginUser = async ({ email, password, role }) => {
-  const res = await axios.post("/api/auth/login", { email, password, role }, {
+  const res = await axios.post(`${API_BASE}/api/auth/login`, {
+    email,
+    password,
+    role,
+  }, {
     withCredentials: true,
   });
-  
-  return res.data;
 
+  return res.data;
 };
